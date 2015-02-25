29a860aae88f25e9e1bec0e3e99fb2fdd2dbdff2
kimveasna <kimveasna@gmail.com>
2015-02-25 22:45:58 +0100

    ajout modèle commande

---
+++ app/src/main/java/com/xyzit/sitcom/model/VModelSalesOrder.java
@@ -0,0 +1,13 @@
+package com.xyzit.sitcom.model;
+import org.robobinding.annotation.PresentationModel;
+
+/**
+ * Created by kimveasna on 22/02/2015.
+ */
+@PresentationModel
+public class VModelSalesOrder
+{
+	private int documentNumber;
+	
+	private String status;
+}
