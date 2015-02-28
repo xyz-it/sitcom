1acbc3bc2b82ae0645337884300692745915976a
kimveasna <kimveasna@gmail.com>
2015-02-28 16:48:25 +0100

    rectification champ pose dans frame header

--- app/src/main/java/com/xyzit/sitcom/model/VModelSalesOrder.java
+++ app/src/main/java/com/xyzit/sitcom/model/VModelSalesOrder.java
@@ -1,5 +1,8 @@
 package com.xyzit.sitcom.model;
+
 import org.robobinding.annotation.PresentationModel;
+
+import java.util.Date;
 
 /**
  * Created by kimveasna on 22/02/2015.
@@ -10,4 +13,12 @@ public class VModelSalesOrder
 	private int documentNumber;
 	
 	private String status;
+
+    private String forwardingAgent;
+
+    private Date deliveryDate;
+
+    private boolean applyDeliveryDate;
+
+
 }
