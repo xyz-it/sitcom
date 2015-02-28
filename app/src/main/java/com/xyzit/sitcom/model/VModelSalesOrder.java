dfa5426e5c564517a1d8e061ffcd192cf2724783
kimveasna <kimveasna@gmail.com>
2015-02-28 16:46:33 +0100

    ajout modèle commande

--- app/src/main/java/com/xyzit/sitcom/model/VModelSalesOrder.java
+++ app/src/main/java/com/xyzit/sitcom/model/VModelSalesOrder.java
@@ -1,5 +1,7 @@
 package com.xyzit.sitcom.model;
 import org.robobinding.annotation.PresentationModel;
+import java.util.*;
+
 
 /**
  * Created by kimveasna on 22/02/2015.
@@ -10,4 +12,39 @@ public class VModelSalesOrder
 	private int documentNumber;
 	
 	private String status;
+	
+	private Date creationDate;
+	private Date modificationDate;
+	private String customerId;
+	private String salesOrganizationId;
+	private String distributionChannelId;
+	private String divisionId;
+	
+	private String salesOfficeId;
+	private String deliveringPlantId;
+	private String customerTaxClassification;
+	
+	private VModelSalesOrderCustomer customer;
+
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+
+	public String getStatus()
+	{
+		return status;
+	}
+
+
+	public void setDocumentNumber(int documentNumber)
+	{
+		this.documentNumber = documentNumber;
+	}
+
+	public int getDocumentNumber()
+	{
+		return documentNumber;
+	}
+	
 }
---
+++ app/src/main/java/com/xyzit/sitcom/model/VModelSalesOrderCustomer.java
@@ -0,0 +1,13 @@
+package com.xyzit.sitcom.model;
+
+import org.robobinding.annotation.PresentationModel;
+
+@PresentationModel
+public class VModelSalesOrderCustomer{
+
+	private String customerId;
+	private String billToPartyId;
+	private String shipToPartyId;
+
+
+}
